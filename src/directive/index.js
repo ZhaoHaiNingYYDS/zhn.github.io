b3d963fca19bc42db6cba38c9ccc4a9645ef8f29
zhaohaining123 <2094747872@qq.com>
2024-09-14 15:52:22 +0800

    提交

--- src/directive/index.js
+++
@@ -1,6 +0,0 @@
-import copyText from './common/copyText'
-import drag from './drag/index'
-export default function directive(app) {
-  app.directive('copyText', copyText)
-  app.directive('drag', drag)
-}
